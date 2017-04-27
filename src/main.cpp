ad3a0c131d4368cd057c8b77e575cc87c3f397bd
dargonaxxe <dargonaxxe@gmail.com>
2017-04-27 13:34:11 +0700

    removed unused library mentions

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
 #include <gmp.h>
 #include "Number.hpp"
-#include <ctime>
-#include <Eigen/Dense>
 
 #define n 100
 
-using namespace Eigen;
+// TODO: Implement our own linear algebra library cuz we got some troubles with open-source libs
 
 int main() {
-//    int const n = 100;
-    Matrix<Number, 50, 50> B;
-    Matrix<Number, 50, 50> A;
-    for (int i = 0; i < 50; i++)
-        for (int j = 0; j < 50; j++) {
-            A(i, j) = Number::getRandom();
-            B(i, j) = Number::getRandom();
-        }
-
-    A *= B;
 
     return 0;
 }
